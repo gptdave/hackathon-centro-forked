eb2fadf9f3a7ddacf7a0d73ec80ca97d6ee03b36
David Cuellar 2 <cuellardave.dev@outlook.com>
2024-11-14 19:43:51 -0600

    Update chat.tsx

--- components/custom/chat.tsx
+++ components/custom/chat.tsx
@@ -166,7 +166,7 @@ export function Chat({
               components={{
                 code({ node, className, children, ...props }) {
                   const match = /language-(\w+)/.exec(className || "");
-                  return  match ? (
+                  return match ? (
                     <SyntaxHighlighter
                       style={atomDark}
                       language={match[1]}
